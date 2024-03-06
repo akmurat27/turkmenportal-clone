3b7202805ae6f8d40e439f16f610b78ff8e46fcc
akmurat27 <akmuratagageldiyev@gmail.com>
2024-03-06 14:48:31 +0500

    second part started

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="hello">
-    <img src="@/assets/img/logo_tp.png">
+  <div class="w-[80%] m-auto">
+    <div class="flex">
+      <img src="@/assets/img/logo_tp.png">
+      <img class="rounded-lg ml-8 mb-3" src="@/assets/img/3cc278f317104ffe0e261fdbf032062c.gif">
+    </div>
     <div class="nav">
-      <div class="unordered-list">
+      <div class="flex justify-around items-center bg-red-500 text-white h-10">
           
         <span>ВСЕ КАТЕГОРИИ</span>
         <span>НОВОСТИ</span>
@@ -17,7 +20,7 @@
       </div>
     </div>
     <div class="second-nav">
-      <div class="blog">
+      <div class="flex justify-around items-center h-10">
         <span>НОВОСТИ</span>
         <span>Дайджест</span>
         <span>Политика</span>
@@ -30,13 +33,62 @@
         <span>Паказать еще</span>
       </div>
     </div>
-    <div class="first-section">
-      <img src="@/assets/img/de579dad30a2e6e4b5cf2019bb541791-8966694-380x253-2.webp">
-      <span>Сегодня 19:47</span>
-      <h1>В Туркменистане стартовала «Академия для женщин-предпринимателей»</h1>
-      <p>В Ашхабаде, Дашогузе, Мары и Туркменабате 2 марта стартовали курсы «Академии для женщин-предпринимателей» (AWE). Программа реализуется Посольством США в Туркменистане совместно с Союзом...</p>
-    <div class="second-section">
-                                                                                                                                                                                       
+    <hr>
+    <div class="flex">
+      <div class="w-[40%] mt-3 m-auto">
+        <img src="@/assets/img/de579dad30a2e6e4b5cf2019bb541791-8966694-380x253-2.webp">
+        <span>Сегодня 19:47</span>
+        <h1>В Туркменистане стартовала «Академия для женщин-предпринимателей»</h1>
+        <p>В Ашхабаде, Дашогузе, Мары и Туркменабате 2 марта стартовали курсы «Академии для женщин-предпринимателей» (AWE). Программа реализуется Посольством США в Туркменистане совместно с Союзом...</p>
+      </div>
+      <div class="w-[45%] mt-3 m-auto">
+        <div class="flex justify-center items-center">
+          <img class="rounded-lg" src="@/assets/img/45b954d88b1176b04e350fcede256acb-8966978-90x60-2.webp">
+          <div class="flex flex-col text-left ml-3">
+            <span class="text-red-800 text-xs my-2">Сегодня 15:53</span>
+            <p class="text-grey-300 text-xs">В агентстве «Туркменсвязь» прошло торжественное собрание по случаю Международного женского дня</p>
+          </div>
+        </div>
+        <div class="flex">
+          <img class="rounded-lg" src="@/assets/img/88f4b801126c79e1b11fb6e589ece9a7-8966890-90x60-2.webp">
+          <div class="flex flex-col text-left ml-3">
+            <span class="text-red-800 text-xs my-2">Сегодня 15:35</span>
+            <p class="text-grey-300 text-xs">Во Французском институте в Ашхабаде покажут фильм «Дивертисмент»</p>
+          </div>
+        </div>
+        <div class="flex justify-center items-center">
+          <img class="rounded-lg" src="@/assets/img/a067b8d535ef634dcde23afc55880a76-8966863-90x60-2.webp">
+          <div class="flex flex-col text-left ml-3">
+            <span class="text-red-800 text-xs my-2">Сегодня 14:04</span>
+            <p class="text-grey-300 text-xs">Главы миссий ОБСЕ обсудили в Ашхабаде региональные вопросы сотрудничества</p>
+          </div>
+        </div>
+        <div class="flex">
+          <img class="rounded-lg" src="@/assets/img/cb9014474986adc597add65a2ef8caf0-8966954-90x60-2.webp">
+          <div class="flex flex-col text-left ml-3">
+            <span class="text-red-800 text-xs my-2">Сегодня 13:17</span>
+            <p class="text-grey-300 text-xs">В Анау состоится ярмарка вакансий</p>
+          </div>
+        </div>
+        <div class="flex justify-center items-center">
+          <img class="rounded-lg" src="@/assets/img/d966ec6bcbc4ff20b196db68049b3e5c-8966979-90x60-2.webp">
+          <div class="flex flex-col text-left ml-3">
+            <span class="text-red-800 text-xs my-2">Сегодня 12:56</span>
+            <p class="text-grey-300 text-xs">Делегация Японского агентства международного сотрудничества посетила порт Туркменбаши</p>
+          </div>
+        </div>
+        <div class="flex justify-center items-center">
+          <img class="rounded-lg" src="@/assets/img/dbae76e6f716d7a53d9e64683fbad462-8966878-90x60-2.webp">
+          <div class="flex flex-col text-left ml-3">
+            <span class="text-red-800 text-xs my-2">Сегодня 12:30</span>
+            <p class="text-grey-300 text-xs">Посол Туркменистана в Румынии и ректор нефтегазового вуза Плоешти обсудили дальнейшее сотрудничество</p>
+          </div>
+        </div>
+      </div>
+      <div class="mt-3">
+        <img class="rounded-lg" src="@/assets/img/53daeb106dab95f9c75ffc67d6f5c2d8.gif">
+        <img class="my-3" src="@/assets/img/videoframe_3341.png">
+      </div>
     </div>
   </div>
 </template>
@@ -53,14 +105,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.unordered-list{
-    width: 80%;
-    height: 50px;
-    margin: auto;
-    background-color: rgb(156, 50, 50);
-    color: white;
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-}
+
 </style>
